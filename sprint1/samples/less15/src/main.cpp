1f6e626f3d4d915614a929ede4c51ab8f6cdcf18
lookson111 <karl.komarblch@gmail.com>
2023-05-29 13:31:27 +0000

    Added lesson 15 sprint 1

---
+++ sprint1/samples/less15/src/main.cpp
@@ -0,0 +1,47 @@
+// Подключаем заголовочный файл <sdkddkver.h> в системе Windows,
+// чтобы избежать предупреждения о неизвестной версии Platform SDK,
+// когда используем заголовочные файлы библиотеки Boost.Asio
+#ifdef WIN32
+#include <sdkddkver.h>
+#endif
+
+#include <boost/asio.hpp>
+#include <iostream>
+#include <thread>
+
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
+using namespace std::literals;
+
+int main() {
+    net::io_context io;
+
+    net::post(io, [&io] {  // (1)
+        std::cout << 'A';
+        net::post(io, [] {  // (2)
+            std::cout << 'B';
+        });
+        std::cout << 'C';
+    });
+
+    net::dispatch(io, [&io] {  // (3)
+        std::cout << 'D';
+        net::post(io, [] {  // (4)
+            std::cout << 'E';
+        });
+        net::defer(io, [] {  // (5)
+            std::cout << 'F';
+        });
+        net::dispatch(io, [] {  // (6)
+            std::cout << 'G';
+        });
+        std::cout << 'H';
+    });
+
+    std::cout << 'I';
+    io.run();
+    std::cout << 'J';
+    return 0;
+} 
+
+ 
