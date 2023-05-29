47b3ca443ce42c7a09d65a4f855f40ed0a9eb108
lookson111 <karl.komarblch@gmail.com>
2023-05-29 13:46:54 +0000

    Lesson 15 commit 2

--- sprint1/samples/less15/src/main.cpp
+++ sprint1/samples/less15/src/main.cpp
@@ -8,11 +8,55 @@
 #include <boost/asio.hpp>
 #include <iostream>
 #include <thread>
-
+#include <syncstream>
 namespace net = boost::asio;
 using tcp = net::ip::tcp;
 using namespace std::literals;
 
+// Run func fn in n thread, with this
+template <typename Fn>
+void RunWorkers(unsigned n, const Fn& fn) {
+    n = std::max(1u, n);
+    std::vector<std::jthread> workers;
+    workers.reserve(n - 1);
+    while (--n) {
+        workers.emplace_back(fn);
+    }
+    fn();
+}
+
+int main() {
+    using osync = std::osyncstream;
+
+    net::io_context io;
+    auto strand = net::make_strand(io);
+
+    net::post(strand, [strand] {
+        net::post(strand, [] {
+            osync(std::cout) << 'A';
+        });
+        net::dispatch(strand, [] {
+            osync(std::cout) << 'B';
+        });
+        osync(std::cout) << 'C';
+    });
+
+    net::post(io, [&io] {
+        net::post(io, [] {
+            osync(std::cout) << 'D';
+        });
+        net::dispatch(io, [] {
+            osync(std::cout) << 'E';
+        });
+        osync(std::cout) << 'F';
+    });
+
+    RunWorkers(2, [&io] {
+        io.run();
+    });
+} 
+// --- 1 ----
+/*
 int main() {
     net::io_context io;
 
@@ -43,5 +87,5 @@ int main() {
     std::cout << 'J';
     return 0;
 } 
-
+*/
  
