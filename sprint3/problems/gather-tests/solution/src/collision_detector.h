395cd4a17ad28caca6bc44860f09f4670c17c2d6
lookson111 <karl.komarblch@gmail.com>
2023-07-18 17:52:29 +0000

    S3T4L4 Fix build project

--- sprint3/problems/gather-tests/solution/src/collision_detector.h
+++ sprint3/problems/gather-tests/solution/src/collision_detector.h
@@ -11,31 +11,25 @@ struct CollectionResult {
     bool IsCollected(double collect_radius) const {
         return proj_ratio >= 0 && proj_ratio <= 1 && sq_distance <= collect_radius * collect_radius;
     }
-    // квадрат расстояния до точки
+
+    // Квадрат расстояния до точки
     double sq_distance;
-    // доля пройденного отрезка
+    // Доля пройденного отрезка
     double proj_ratio;
 };
 
-// Движемся из точки a в точку b и пытаемся подобрать точку c.
-// Эта функция реализована в уроке.
+// Движемся из точки a в точку b и пытаемся подобрать точку c
 CollectionResult TryCollectPoint(geom::Point2D a, geom::Point2D b, geom::Point2D c);
 
 struct Item {
     geom::Point2D position;
     double width;
-    friend bool operator==(const Item& lh, const Item &rh) {
-        return lh.position == rh.position && lh.width == rh.width;
-    }
 };
 
 struct Gatherer {
     geom::Point2D start_pos;
     geom::Point2D end_pos;
     double width;
-    friend bool operator==(const Gatherer& lh, const Gatherer &rh) {
-        return lh.start_pos == rh.start_pos && lh.end_pos == rh.end_pos && lh.width == rh.width;
-    }
 };
 
 class ItemGathererProvider {
@@ -49,24 +43,6 @@ public:
     virtual Gatherer GetGatherer(size_t idx) const = 0;
 };
 
-class ItemGatherer : ItemGathererProvider {
-public:
-    using Items = std::vector<Item>;
-    using Gatherers = std::vector <Gatherer>;
-
-    void Set(const Gatherers &gatherers);
-    void Set(const Items &items);
-
-    virtual size_t ItemsCount() const override;
-    virtual Item GetItem(size_t idx) const override;
-    virtual size_t GatherersCount() const override;
-    virtual Gatherer GetGatherer(size_t idx) const override;
-
-private:
-    std::vector<Item> items_;
-    std::vector<Gatherer> gatherers_;
-};
-
 struct GatheringEvent {
     size_t item_id;
     size_t gatherer_id;
@@ -74,8 +50,11 @@ struct GatheringEvent {
     double time;
 };
 
-// Эту функцию вам нужно будет реализовать в соответствующем задании.
-// При проверке ваших тестов она не нужна - функция будет линковаться снаружи.
 std::vector<GatheringEvent> FindGatherEvents(const ItemGathererProvider& provider);
+std::vector<GatheringEvent> FindGatherEvents_Wrong1(const ItemGathererProvider& provider);
+std::vector<GatheringEvent> FindGatherEvents_Wrong2(const ItemGathererProvider& provider);
+std::vector<GatheringEvent> FindGatherEvents_Wrong3(const ItemGathererProvider& provider);
+std::vector<GatheringEvent> FindGatherEvents_Wrong4(const ItemGathererProvider& provider);
+std::vector<GatheringEvent> FindGatherEvents_Wrong5(const ItemGathererProvider& provider);
 
 }  // namespace collision_detector
--- sprint3/problems/gather-tests/solution/tests/collision-detector-tests.cpp
+++ sprint3/problems/gather-tests/solution/tests/collision-detector-tests.cpp
@@ -17,6 +17,44 @@ namespace Catch {
         }
     };
 }  // namespace Catch 
+namespace collision_detector {
+class ItemGatherer : ItemGathererProvider {
+public:
+    using Items = std::vector<Item>;
+    using Gatherers = std::vector <Gatherer>;
+
+    void Set(const Gatherers &gatherers){
+        gatherers_ = gatherers;
+    }
+    void Set(const Items &items) {
+        items_ = items;
+    }
+    virtual size_t ItemsCount() const override {
+        return items_.size();
+    }
+    virtual Item GetItem(size_t idx) const override {
+        return items_[idx];
+    }
+    virtual size_t GatherersCount() const override {
+        return gatherers_.size();
+    }
+    virtual Gatherer GetGatherer(size_t idx) const override {
+        return gatherers_[idx];
+    }
+
+private:
+    std::vector<Item> items_;
+    std::vector<Gatherer> gatherers_;
+};
+}
+bool operator==(const Item& lh, const Item &rh) {
+        return lh.position == rh.position && lh.width == rh.width;
+}
+bool operator==(const Gatherer& lh, const Gatherer &rh) {
+        return lh.start_pos == rh.start_pos && lh.end_pos == rh.end_pos && lh.width == rh.width;
+}
+
+
 static const ItemGatherer::Items one_item = { Item{.position{0.0, 5.0}, .width = 0.2 } };
 static const ItemGatherer::Gatherers one_gatherer =
     { Gatherer{.start_pos{0.0, 0.0}, .end_pos{0.0, 10.0}, .width = 0.3} };
