9842814cb83d91c265c2ee76f124decbb64e58eb
lookson111 <karl.komarblch@gmail.com>
2023-08-03 20:06:07 +0000

    S4T2L7 Initial commit

---
+++ sprint4/problems/bookypedia-1/solution/src/postgres/postgres.cpp
@@ -0,0 +1,58 @@
+#include "postgres.h"
+
+#include <pqxx/zview.hxx>
+
+namespace postgres {
+
+using namespace std::literals;
+using pqxx::operator"" _zv;
+
+void AuthorRepositoryImpl::Save(const domain::Author& author) {
+    // Пока каждое обращение к репозиторию выполняется внутри отдельной транзакции
+    // В будущих уроках вы узнаете про паттерн Unit of Work, при помощи которого сможете несколько
+    // запросов выполнить в рамках одной транзакции.
+    // Вы также может самостоятельно почитать информацию про этот паттерн и применить его здесь.
+    pqxx::work work{connection_};
+    work.exec_params(
+        R"(
+INSERT INTO authors (id, name) VALUES ($1, $2)
+ON CONFLICT (id) DO UPDATE SET name=$2;
+)"_zv,
+        author.GetId().ToString(), author.GetName());
+    work.commit();
+}
+
+void AuthorRepositoryImpl::GetAuthors(domain::Authors &autors) {
+    // Пока каждое обращение к репозиторию выполняется внутри отдельной транзакции
+    // В будущих уроках вы узнаете про паттерн Unit of Work, при помощи которого сможете несколько
+    // запросов выполнить в рамках одной транзакции.
+    // Вы также может самостоятельно почитать информацию про этот паттерн и применить его здесь.
+    pqxx::read_transaction r{connection_};
+    auto query_text = R"(SELECT id, name FROM authors 
+ORDER BY name DESC;
+)"_zv;
+    for (auto [id, name] : 
+            r.query<std::string, std::string>(R"(SELECT id, name FROM authors 
+ORDER BY name DESC;
+)"_zv)) {
+        //domain::Author author(domain::AuthorId::FromString(id), name);
+        //autors.push_back(std::move(author));
+    }
+}
+
+Database::Database(pqxx::connection connection)
+    : connection_{std::move(connection)} {
+    pqxx::work work{connection_};
+    work.exec(R"(
+CREATE TABLE IF NOT EXISTS authors (
+    id UUID CONSTRAINT author_id_constraint PRIMARY KEY,
+    name varchar(100) UNIQUE NOT NULL
+);
+)"_zv);
+    // ... создать другие таблицы
+
+    // коммитим изменения
+    work.commit();
+}
+
+}  // namespace postgres
