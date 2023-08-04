af6935b4df48641f6c746dcdff8a491c64196b37
lookson111 <karl.komarblch@gmail.com>
2023-08-04 12:49:12 +0000

    S4T2L7 Add show authors

--- sprint4/problems/bookypedia-1/solution/src/postgres/postgres.cpp
+++ sprint4/problems/bookypedia-1/solution/src/postgres/postgres.cpp
@@ -44,6 +44,14 @@ CREATE TABLE IF NOT EXISTS authors (
     name varchar(100) UNIQUE NOT NULL
 );
 )"_zv);
+    work.exec(R"(
+CREATE TABLE IF NOT EXISTS books (
+    id                  UUID CONSTRAINT book_id_constraint PRIMARY KEY,
+    author_id           UUID REFERENCES authors (id) NOT NULL, 
+    title               varchar(100) NOT NULL,
+    publication_year    integer,
+);
+)"_zv);
     // ... создать другие таблицы
 
     // коммитим изменения
