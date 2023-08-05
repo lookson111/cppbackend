86029684a9e0d6b2ef94d333958f78104b1fb107
lookson111 <karl.komarblch@gmail.com>
2023-08-05 10:15:41 +0000

    S4T3L3 Initial commit 2

---
+++ sprint4/problems/bookypedia-2/solution/src/ui/detail.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace ui {
+namespace detail {
+
+struct AddBookParams {
+    std::string title;
+    std::string author_id;
+    int publication_year = 0;
+};
+
+struct AuthorInfo {
+    std::string id;
+    std::string name;
+};
+
+struct BookInfo {
+    std::string title;
+    int publication_year;
+};
+
+using AuthorsInfo = std::vector<AuthorInfo>;
+using BooksInfo = std::vector<BookInfo>;
+
+} // namespace detail
+} // namespace ui
---
+++ sprint4/problems/bookypedia-2/solution/tests/use_case_tests.cpp
@@ -0,0 +1,58 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "../src/app/use_cases_impl.h"
+#include "../src/domain/book.h"
+
+namespace {
+
+struct MockAuthorRepository : domain::AuthorRepository {
+    std::vector<domain::Author> saved_authors;
+
+    void Save(const domain::Author& author) override {
+        saved_authors.emplace_back(author);
+    }
+    void GetAuthors(domain::Authors& authors) override {
+        saved_authors.clear();
+        for (auto author : authors)
+            saved_authors.emplace_back(author);
+    }
+};
+
+struct MockBookRepository : domain::BookRepository {
+    std::vector<domain::Book> saved_books;
+
+    void Save(const domain::Book& book) override {
+        saved_books.emplace_back(book);
+    }
+    domain::Books GetAuthorBooks(const domain::AuthorId& author_id) override {
+        return saved_books;
+    }
+    domain::Books GetBooks() override {
+        return saved_books;
+    }
+
+};
+
+struct Fixture {
+    MockAuthorRepository authors;
+    MockBookRepository books;
+};
+
+}  // namespace
+
+SCENARIO_METHOD(Fixture, "Book Adding") {
+    GIVEN("Use cases") {
+        app::UseCasesImpl use_cases{authors, books};
+
+        WHEN("Adding an author") {
+            const auto author_name = "Joanne Rowling";
+            use_cases.AddAuthor(author_name);
+
+            THEN("author with the specified name is saved to repository") {
+                REQUIRE(authors.saved_authors.size() == 1);
+                CHECK(authors.saved_authors.at(0).GetName() == author_name);
+                CHECK(authors.saved_authors.at(0).GetId() != domain::AuthorId{});
+            }
+        }
+    }
+}
