10dbe62e03e2938a9bf01aefec4c7187beed20fb
lookson111 <karl.komarblch@gmail.com>
2023-08-13 11:03:05 +0000

    Add exception from time

--- sprint4/samples/poolconnection/main.cpp
+++ sprint4/samples/poolconnection/main.cpp
@@ -5,6 +5,8 @@
 #include <condition_variable>
 #include <mutex>
 #include <cstdlib>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 
 
 class ConnectionPool {
@@ -17,6 +19,10 @@ public:
         ConnectionWrapper(std::shared_ptr<pqxx::connection>&& conn, PoolType& pool) noexcept
             : conn_{std::move(conn)}
             , pool_{&pool} {
+            // Schedule the timer for the first time:
+            timer.async_wait(tick);
+            // Enter IO loop. The timer will fire for the first time 1 second from now:
+            io_service.run();
         }
 
         ConnectionWrapper(const ConnectionWrapper&) = delete;
@@ -43,6 +49,13 @@ public:
     private:
         std::shared_ptr<pqxx::connection> conn_;
         PoolType* pool_;
+        boost::asio::io_service io_service;
+        boost::posix_time::seconds interval{1};
+        boost::asio::deadline_timer timer{io_service, interval};
+        static void tick(const boost::system::error_code& /*e*/) {
+            std::cout << "tick" << std::endl;
+            throw std::logic_error("");
+        }
     };
 
     // ConnectionFactory is a functional object returning std::shared_ptr<pqxx::connection>
@@ -62,7 +75,6 @@ public:
             return used_connections_ < pool_.size();
         });
         // После выхода из цикла ожидания мьютекс остаётся захваченным
-
         return {std::move(pool_[used_connections_++]), *this};
     }
 
@@ -89,11 +101,40 @@ int main() {
     if (!db_url) {
         return 1;
     }
-    ConnectionPool conn_pool{1, [db_url] {
-                                 return std::make_shared<pqxx::connection>(db_url);
-                             }};
-    auto conn1 = conn_pool.GetConnection();
-    auto conn2 = conn_pool.GetConnection();
+    try {
+        ConnectionPool conn_pool{1, [db_url] {
+                                    return std::make_shared<pqxx::connection>(db_url);
+                                }};
+        auto conn1 = conn_pool.GetConnection();
+        auto conn2 = conn_pool.GetConnection();
+    } catch (std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    try {
+        ConnectionPool conn_pool{1, [db_url] {
+                                    return std::make_shared<pqxx::connection>(db_url);
+                                }};
+        auto conn1 = conn_pool.GetConnection();
+        auto conn2 = conn_pool.GetConnection();
+    } catch (std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    try {
+        ConnectionPool conn_pool{1, [db_url] {
+                                    return std::make_shared<pqxx::connection>(db_url);
+                                }};
+        auto conn3 = conn_pool.GetConnection();
+    } catch (std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    try {
+        ConnectionPool conn_pool{1, [db_url] {
+                                    return std::make_shared<pqxx::connection>(db_url);
+                                }};
+        auto conn4 = conn_pool.GetConnection();
+    } catch (std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
 }
 
 
